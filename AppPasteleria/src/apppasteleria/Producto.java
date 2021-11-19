e0cd7f6ac2e0bcca860676f02ac3a3470df3004b
Leandro Dzib <73036720+leandrodzn@users.noreply.github.com>
2021-11-18 18:26:37 -0600

    Producto

---
+++ AppPasteleria/src/apppasteleria/Producto.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author leodz
+ */
+public class Producto {
+    protected String ID;
+    protected String tipo;
+    protected String nombre;
+    protected Double precio;
+    protected Integer cantidad;
+    
+    public Producto(){}
+
+    public Producto(String ID, String tipo, String nombre, Double precio, Integer cantidad){
+        this.ID = ID;
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getID() {
+        return this.ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    
+}
