f8799db45dc7f654c01496754367ae30c8a92b90
erikpsanchez <erikpootsanche@gmail.com>
2021-11-18 22:41:10 -0600

    Agrega documentacion en Javadoc

--- AppPasteleria/src/apppasteleria/modelo/Persona.java
+++ AppPasteleria/src/apppasteleria/modelo/Persona.java
@@ -13,35 +13,64 @@ public class Persona {
     private String ID;
     private String nombre;
     private String apellido;
-    
+
+/**
+ * Constructor por defecto de la clase Persona
+ */    
     public Persona() {}
-    
+
+/**
+ * Constructor de la clase Persona
+ * @param ID
+ * @param nombre
+ * @param apellido 
+ */    
     public Persona(String ID, String nombre, String apellido) {
         this.ID = ID;
         this.nombre = nombre;
         this.apellido = apellido;
     }
-    
+/**
+ * Método que asigna un valor al atributo ID
+ * @param ID 
+ */    
     public void setID(String ID) {
         this.ID = ID;
     }
-    
+
+/**
+ * Método que devuelve el valor del atributo ID
+ * @return 
+ */    
     public String getID() {
         return this.ID;
     }
-    
+
+/**
+ * Método que asigna un valor al atributo nombre
+ * @param nombre 
+ */    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
+
+/**
+ * Método que devuelve el valor del atributo nombre
+ * @return 
+ */    
     public String getNombre() {
         return this.nombre;
     }
-    
+
+//Método que asigna un valor al atributo apellido    
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
     
+/**
+ * Método que devuelve el valor del atributo apellido
+ * @return 
+ */
     public String getApellido() {
         return this.apellido;
     }
