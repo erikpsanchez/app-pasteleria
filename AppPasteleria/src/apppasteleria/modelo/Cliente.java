58e95956ff4372e96128a8b0297a8727f4e09647
erikpsanchez <erikpootsanche@gmail.com>
2021-11-18 23:05:14 -0600

    Agrega documentacion en Javadoc

--- AppPasteleria/src/apppasteleria/modelo/Cliente.java
+++ AppPasteleria/src/apppasteleria/modelo/Cliente.java
@@ -11,24 +11,49 @@ package apppasteleria;
  */
 public class Cliente extends Persona {
     private Integer sweetpoints;
-    
+
+/**
+ * Constructor por defecto de la clase Cliente
+ */    
     public Cliente() {}
     
+/**
+ * Constructor de la clase Cliente
+ * @param ID
+ * @param nombre
+ * @param apellido
+ * @param sweetpoints 
+ */    
     public Cliente(String ID, String nombre, String apellido, Integer sweetpoints) {
         super(ID, nombre, apellido);
         this.sweetpoints = sweetpoints;
     }
-    
+
+/**
+ * Método que asigna un valor al atributo sweetpoints
+ * @param sweetpoints 
+ */    
     public void setSweetpoints(Integer sweetpoints) {
         this.sweetpoints = sweetpoints;
     }
-    
+
+/**
+ * Método que devuelve el valor del atributo sweetpoints
+ * @return sweetpoints
+ */    
     public Integer getSweetpoints() {
         return this.sweetpoints;
     }
     
+/**
+ * Método que muestra la información de un objeto de la clase Cliente
+ * @return String con los valores de todos los atributos de un objeto de la clase Cliente
+ */
     @Override
     public String toString() {
-        return "";
+        return  "ID: " + getID() +
+                "Nombre: " + getNombre() +
+                "Apellido: " + getApellido() + 
+                "Sweetpoints: " + getSweetpoints();
     }
 }
