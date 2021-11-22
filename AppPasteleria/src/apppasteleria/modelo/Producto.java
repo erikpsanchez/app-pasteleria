f068ffe5b35fddb4e18f0556c4124906ab71a939
Carlos Sánchez <carlosfsanchezchuc@gmail.com>
2021-11-22 06:25:05 -0600

    test-persona

--- AppPasteleria/src/apppasteleria/modelo/Producto.java
+++ AppPasteleria/src/apppasteleria/modelo/Producto.java
@@ -5,6 +5,7 @@
  */
 package modelo;
 
+
 /**
  *
  * @author leodz
--- AppPasteleria/test/apppasteleria/controlador/ControladorPersonaTest.java
+++ AppPasteleria/test/apppasteleria/controlador/ControladorPersonaTest.java
@@ -5,11 +5,6 @@
  */
 package apppasteleria.controlador;
 
-import apppasteleria.Persona;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
