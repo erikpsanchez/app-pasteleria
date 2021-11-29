4b9dbe51a476cf5d862d980bc12c7d74f08fc1be
leandrodzn <leodzib100@gmail.com>
2021-11-29 16:08:05 -0600

    registro clientes

--- AppPasteleria/src/apppasteleria/controlador/AppPasteleria.java
+++ AppPasteleria/src/apppasteleria/controlador/AppPasteleria.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package apppasteleria;
+package apppasteleria.controlador;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -19,12 +20,19 @@ public class AppPasteleria extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(AppPasteleria.class.getResource("/vista/Vista.fxml"));
+            Pane ventana = (Pane) loader.load();
+            
+            Scene scene = new Scene(ventana);
+            
+            primaryStage.setTitle("Tabla");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     /**
