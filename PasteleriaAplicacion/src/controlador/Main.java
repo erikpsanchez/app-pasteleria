67ff7ab21b14aed6a4218f50889ec11aaed33897
leandrodzn <leodzib100@gmail.com>
2021-12-01 17:15:54 -0600

    ojala2

--- PasteleriaAplicacion/src/controlador/Main.java
+++ PasteleriaAplicacion/src/controlador/Main.java
@@ -27,7 +27,7 @@ public class Main extends Application{
     public void start(Stage primaryStage){
         try{
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("/vista/VistaRegistrarCliente.fxml"));
+            loader.setLocation(Main.class.getResource("/vista/VistaRegistrarAdministrador.fxml"));
             Pane ventana = (Pane) loader.load();
             
             Scene scene = new Scene(ventana);
