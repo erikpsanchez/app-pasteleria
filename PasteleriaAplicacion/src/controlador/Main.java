a77099a876e46b12597bf2ec933edded45526c8e
erikpsanchez <erikpootsanche@gmail.com>
2021-12-01 19:28:18 -0600

    Registro de empleados

--- PasteleriaAplicacion/src/controlador/Main.java
+++ PasteleriaAplicacion/src/controlador/Main.java
@@ -28,9 +28,11 @@ public class Main extends Application{
         try{
             FXMLLoader loader = new FXMLLoader();
 
-            loader.setLocation(Main.class.getResource("/vista/VistaRegistrarCliente.fxml"));
+            //loader.setLocation(Main.class.getResource("/vista/VistaRegistrarCliente.fxml"));
             //loader.setLocation(Main.class.getResource("/vista/RegistrarPasteles.fxml"));
             //loader.setLocation(Main.class.getResource("/vista/RegistrarPostresDelicias.fxml"));
+            //loader.setLocation(Main.class.getResource("/vista/VistaRegistrarAdministrador.fxml"));
+            loader.setLocation(Main.class.getResource("/vista/VistaRegistrarEmpleado.fxml"));
 
             Pane ventana = (Pane) loader.load();
             
